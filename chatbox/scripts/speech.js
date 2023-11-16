8770373e7a240bd771a424036e51e41751aa2623
Sia-WRWD <chinojen7@gmail.com>
2023-11-16 12:26:12 +0800

    Edited speech.js, working but encounters error of worker unavailability.

--- chatbox/scripts/speech.js
+++ chatbox/scripts/speech.js
@@ -1,46 +1,54 @@
 const fs = require("fs");
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
-
 const config = require('./config');
+
 const endpoint = config.AZURE_SPEECH_ENDPOINT;
 const azureApiKey = config.AZURE_SPEECH_KEY;
 const region = config.AZURE_SPEECH_REGION;
 
-// This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
 const speechConfig = sdk.SpeechConfig.fromSubscription(azureApiKey, region);
 speechConfig.speechRecognitionLanguage = "en-US";
-
-// Requests word-level timestamps for the recognized speech.
 speechConfig.requestWordLevelTimestamps();
-
-// Sets the output format to Detailed.
 speechConfig.outputFormat = sdk.OutputFormat.Detailed;
 
 export const fromFile = async (audioFile) => {
-    let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync(audioFile));
-    let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-    speechRecognizer.recognizeOnceAsync(result => {
-        switch (result.reason) {
-            case sdk.ResultReason.RecognizedSpeech:
-                return `RECOGNIZED: Text=${result.text}`;
-            case sdk.ResultReason.NoMatch:
-                return "NOMATCH: Speech could not be recognized.";
-            case sdk.ResultReason.Canceled:
-                const cancellation = sdk.CancellationDetails.fromResult(result);
-                var reason = `CANCELED: Reason=${cancellation.reason}`;
+  return new Promise((resolve, reject) => {
+    try {
+      const audioConfig = sdk.AudioConfig.fromWavFileInput(audioFile);
+      const speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
 
-                if (cancellation.reason == sdk.CancellationReason.Error) {
-                    return `
-                    CANCELED: Reason=${cancellation.reason}
-                    CANCELED: ErrorCode=${cancellation.ErrorCode}
-                    CANCELED: ErrorDetails=${cancellation.errorDetails}
-                    CANCELED: Did you set the speech resource key and region values?"
-                    `;
-                } else {
-                    return reason;
-                }
+      speechRecognizer.recognizeOnceAsync(result => {
+        switch (result.reason) {
+          case sdk.ResultReason.RecognizedSpeech:
+            const recognizedText = `RECOGNIZED: Text=${result.text}`;
+            resolve(recognizedText);
+            console.log(recognizedText); // Add console.log here
+            break;
+          case sdk.ResultReason.NoMatch:
+            resolve("NOMATCH: Speech could not be recognized.");
+            break;
+          case sdk.ResultReason.Canceled:
+            const cancellation = sdk.CancellationDetails.fromResult(result);
+            const reason = `CANCELED: Reason=${cancellation.reason}`;
+
+            if (cancellation.reason === sdk.CancellationReason.Error) {
+              reject(`
+                CANCELED: Reason=${cancellation.reason}
+                CANCELED: ErrorCode=${cancellation.ErrorCode}
+                CANCELED: ErrorDetails=${cancellation.errorDetails}
+                CANCELED: Did you set the speech resource key and region values?"
+              `);
+            } else {
+              resolve(reason);
+            }
+            break;
         }
+
+        // Close the speech recognizer
         speechRecognizer.close();
-        console.log("it works somehow.");
-    });
-}
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
