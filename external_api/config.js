7458aab9ba1ef10492fa402404730e4efcd022a2
Sia-WRWD <chinojen7@gmail.com>
2023-11-22 17:17:30 +0800

    Created server for Microsoft Azure SDK STT codes.

---
+++ external_api/config.js
@@ -0,0 +1,7 @@
+const config = {
+    AZURE_SPEECH_ENDPOINT: 'https://japaneast.api.cognitive.microsoft.com/sts/v1.0/issuetoken',
+    AZURE_SPEECH_KEY: '895df0c8e8864ac2ada94f2138b34a62',
+    AZURE_SPEECH_REGION: 'japaneast'
+};
+
+export default config;
---
+++ external_api/server.js
@@ -0,0 +1,64 @@
+import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
+import * as cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import config from './config';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
+
+app.post('/transcribe', async (req, res) => {
+  try {
+    const audioBuffer = req.body;
+
+    // Create the push stream
+    const pushStream = sdk.AudioInputStream.createPushStream();
+
+    // Push audio data to the stream
+    pushStream.write(audioBuffer);
+    pushStream.close();
+
+    // Set up speech config and audio config
+    const speechConfig = sdk.SpeechConfig.fromSubscription(config.AZURE_SPEECH_KEY, config.AZURE_SPEECH_REGION);
+    const audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+
+    // Create the conversation transcriber
+    const transcriber = new sdk.ConversationTranscriber(speechConfig, audioConfig);
+
+    // Define event handlers
+    transcriber.sessionStarted = (s, e) => {
+      console.log(`(sessionStarted) SessionId: ${e.sessionId}`);
+    };
+    transcriber.sessionStopped = (s, e) => {
+      console.log(`(sessionStopped) SessionId: ${e.sessionId}`);
+    };
+    transcriber.canceled = (s, e) => {
+      console.log(`(canceled) ${e.errorDetails}`);
+      res.status(500).json({ error: 'Transcription error' });
+    };
+    transcriber.transcribed = (s, e) => {
+      console.log(`(transcribed) text: ${e.result.text}`);
+      console.log(`(transcribed) speakerId: ${e.result.speakerId}`);
+      res.json({ text: e.result.text, speakerId: e.result.speakerId });
+    };
+
+    // Begin conversation transcription
+    await transcriber.startTranscribingAsync(
+      () => {},
+      (err) => {
+        console.trace(`err - starting transcription: ${err}`);
+        res.status(500).json({ error: 'Transcription error' });
+      }
+    );
+  } catch (error) {
+    console.error(`Unexpected error: ${error.message}`);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
